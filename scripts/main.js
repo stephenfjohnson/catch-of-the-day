aebc8924ecf028b88f6a2ecc60db15fed9fe20c9
Stephen Johnson <sforrestj@gmail.com>
2016-02-17 21:14:50 -0800

    Not Passing
    Uncaught TypeError: Cannot convert undefined or null to object

--- scripts/main.js
+++ scripts/main.js
@@ -20,6 +20,10 @@ var App = React.createClass({
       order : {}
     }
   },
+  addToOrder : function(key) {
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({ order : this.state.order });
+  },
   addFish : function(fish) {
     var timestamp = (new Date()).getTime();
     this.state.fishes['fish-' + timestamp] = fish;
@@ -31,7 +35,8 @@ var App = React.createClass({
     });
   },
   renderFish : function(key) {
-    return <Fish key={key} index={key} details={this.state.fishes[key]}/>
+    return <Fish key={key} index={key} details={this.state.fishes[key]}
+      addToOrder={this.addToOrder}/>
   },
   render : function(){
     return (
@@ -43,6 +48,7 @@ var App = React.createClass({
           </ul>
         </div>
         <Order />
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
@@ -53,8 +59,15 @@ var App = React.createClass({
 // <Fish/>
 
 var Fish = React.createClass({
+  onButtonClick : function() {
+    // console.log("Going to add the fish: ", this.props.index);
+    var key = this.props.index;
+    this.props.addToOrder(key);
+  },
   render : function() {
     var details = this.props.details;
+    var isAvailible = (details.status === 'available' ? true : false);
+    var buttonText = (isAvailible ? 'Add To Order' : 'Sold Out!');
     return (
       <li className="menu-fish">
         <img src={details.image} alt={details.name}/>
@@ -63,6 +76,7 @@ var Fish = React.createClass({
           <span className="price">{h.formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
+        <button disabled={!isAvailible} onClick={this.onButtonClick}>{buttonText}</button>
       </li>
     )
   }
@@ -126,9 +140,36 @@ var Header = React.createClass({
 // <Order/>
 
 var Order = React.createClass({
+  renderOrder : function(key) {
+    return <li>{key}</li>
+  },
   render : function() {
+    console.log(this.props.order);
+    var orderIds = Object.keys(this.props.order);
+
+    var total = orderIds.reduce((prevTotal, key)=> {
+      var fishes = this.props.fish[key];
+      var count = this.props.count[key];
+      var isAvailible = fish && fish.status === 'available';
+
+      if(fish && isAvailible) {
+        return prevTotal + (count * parseInt(fish.price) || 0)
+      }
+
+      return prevTotal;
+    }, 0);
+
     return (
-      <h2>Order</h2>
+      <div className="order-wrap">
+        <h2 className="order-title">Your Order</h2>
+        <ul className="order">
+            {orderIds.map(this.renderOrder)}
+            <li className="total">
+              <strong>Total:</strong>
+              {h,formatPrice(total)}
+            </li>
+        </ul>
+      </div>
     )
   }
 });
